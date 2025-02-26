47894133ff2142ab77fac0405f5f958f8085baa3
Marcobc-exe <marco.developing@gmail.com>
2025-02-26 13:48:04 -0300

    Browser tab title was changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,11 @@ import { HashRouter } from "react-router-dom";
 import { AppRouter } from "./routes/AppRouter";
 
 const App = () => {
-  const listOfMaps = [
-    {
-      id: "0",
-      name: "VEGA BAJA",
-      center: "-32.420329; -70.952348",
-      zoom: 14,
-    },
-    {
-      id: "1",
-      name: "Billy Corn",
-      center: "-32.411042; -71.047160",
-      zoom: 14,
-    }
-  ];
-
-  // localStorage.setItem("maps", JSON.stringify(listOfMaps))
-    
   return (
     <HashRouter>
       <AppRouter />
     </HashRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
